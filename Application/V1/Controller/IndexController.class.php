483a29452dfdb85119bfaa0d936f3dd3544197e3
walle2eve <shmily_dhy@qq.com>
2016-06-30 18:10:58 +0800

    index update

--- Application/V1/Controller/IndexController.class.php
+++ Application/V1/Controller/IndexController.class.php
@@ -7,15 +7,17 @@ class IndexController extends Controller
 {
     public function index(){
       exit('error page');
-		echo "当前模块名:" . MODULE_NAME;
-		echo "<br />";
-		echo "当前模块路径:" . MODULE_PATH;
-		echo "<br />";
-		echo "当前控制器名:" . CONTROLLER_NAME;
-		echo "<br />";
-		echo "当前控制器路径:" . CONTROLLER_PATH;
-		echo "<br />";
-		echo "当前操作名:" . ACTION_NAME;
-		echo "<br />";
+      /**
+  		echo "当前模块名:" . MODULE_NAME;
+  		echo "<br />";
+  		echo "当前模块路径:" . MODULE_PATH;
+  		echo "<br />";
+  		echo "当前控制器名:" . CONTROLLER_NAME;
+  		echo "<br />";
+  		echo "当前控制器路径:" . CONTROLLER_PATH;
+  		echo "<br />";
+  		echo "当前操作名:" . ACTION_NAME;
+  		echo "<br />";
+      **/
     }
 }
