5743c3943ecfbd5e3e534661c98b5bfdbfab0462
walle2eve <shmily_dhy@qq.com>
2016-07-13 12:12:35 +0800

    开启调试模式

--- Application/V1/Controller/BaseController.class.php
+++ Application/V1/Controller/BaseController.class.php
@@ -6,28 +6,28 @@ use Think\Controller\RestController;
 use V1\Service\PlayerService;
 
 class BaseController extends RestController{
-	
+
 	public function _initialize(){
 
 		$this->check_method();
-		
+
 		$this->check_operator();
-		
+
 	}
-	
+
 	private function check_method(){
-		
-		if($this->_method != 'post')exit('error method');
-		
+
+		if(strtolower($this->_method) != 'post')exit('error method');
+
 	}
-	
+
 	// 检测提交的运营商Key
 	private function check_operator(){
 		$operator_id = I('post.operatorid','');
 		$operator_key = I('post.operatorkey','');
-		
+
 		$return = PlayerService::check_operator($operator_id,$operator_key);
-				
+
 		if($return === false){
 			$err_code = 1002;
 			$result = array(
