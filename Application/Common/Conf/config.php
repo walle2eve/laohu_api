3e81de36e73501e87b1119849d8a05809c4cdbdd
walle2eve <shmily_dhy@qq.com>
2017-01-17 11:50:24 +0800

    部分修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -57,31 +57,35 @@ return array(
 	
     /* 数据库设置 */
     'DB_TYPE'                => 'mysql', // 数据库类型
-    'DB_HOST'                => '13.75.121.157', // 服务器地址
-    'DB_NAME'                => 'laohu', // 数据库名
-    'DB_USER'                => 'root', // 用户名
-    'DB_PWD'                 => '55A434B43221A1', // 密码
+    'DB_HOST'                => '119.9.74.11,119.9.76.96', // 服务器地址
     'DB_PORT'                => '3306', // 端口
+    'DB_NAME'                => 'laohu', // 数据库名
+    'DB_USER'                => 'quxiang', // 用户名
+    'DB_PWD'                 => '55CD3F6DF491F945', // 密码
     'DB_PREFIX'              => 't_', // 数据库表前缀
     'DB_PARAMS'              => array(), // 数据库连接参数
-    'DB_DEBUG'               => true, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_DEBUG'               => false, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'        => true, // 启用字段缓存
     'DB_CHARSET'             => 'utf8', // 数据库编码默认采用utf8
-    'DB_DEPLOY_TYPE'         => 0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'DB_RW_SEPARATE'         => false, // 数据库读写是否分离 主从式有效
+    'DB_DEPLOY_TYPE'         => 1, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    'DB_RW_SEPARATE'         => true, // 数据库读写是否分离 主从式有效
     'DB_MASTER_NUM'          => 1, // 读写分离后 主服务器数量
     'DB_SLAVE_NO'            => '', // 指定从服务器序号
-	
-	// log库连接信息
-	'DB_LAOHU_LOG_CONFIG' => array(
-		'db_type'  => 'mysql',
-		'db_user'  => 'root',
-		'db_pwd'   => '55A434B43221A1',
-		'db_host'  => '13.75.121.157',
-		'db_port'  => '3306',
-		'db_name'  => 'laohu_log',
-		'db_charset'=>    'utf8',
-	),
+
+    // log库连接信息
+    'DB_LAOHU_LOG_CONFIG' => array(
+        'db_type'  => 'mysql',
+        'db_host'  => '119.9.74.11,119.9.76.96',
+        'db_port'  => '3306',
+        'db_user'  => 'quxiang',
+        'db_pwd'   => '55CD3F6DF491F945',
+        'db_name'  => 'laohu_log',
+        'db_charset'=>    'utf8',
+        'DB_DEPLOY_TYPE'         => 1, 
+        'DB_RW_SEPARATE'         => true, 
+        'DB_MASTER_NUM'          => 1, 
+        'DB_SLAVE_NO'            => '', 
+    ),
 
     //  MONGO-DB
     'DB_TYPE_MONGO_CONFIG'  => array(
